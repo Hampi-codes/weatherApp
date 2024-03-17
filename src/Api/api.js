22819c6a561e1ba5354de71360ca2179dca01aac
Yogesh Kumar <yogeshkumar9582264326@gmail.com>
2024-03-17 19:02:43 +0530

    uploading to github

---
+++ src/Api/api.js
@@ -0,0 +1,9 @@
+ export const geoApiOptions = {
+    method: 'GET',
+	headers: {
+        'X-RapidAPI-Key': '33cc491508msh1b865ed943e6b29p10c67ajsn52ac491ec620',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
